e757c28d53464163fabdb8fe46e5d60112211abd
sarah-mclaughlin <sarahmclaughlin0@gmail.com>
2018-05-30 12:11:50 +1200

    Completed matrix test - it sux

--- matrix.js
+++ matrix.js
@@ -1,5 +1,21 @@
+module.exports = {
+  getMatrix,
+  updateMatrix
+}
+
 function getMatrix (n) {
+  let newArr = []
+  for (var i = 0; i < n; i++) {
+    let newArr1 = []
+    for (var j = 0; j < n; j++) {
+      newArr1.push(0)
+    }
+    newArr.push(newArr1)
+  }
+  return newArr
 }
 
 function updateMatrix (matrix, coords, value) {
+  matrix[coords[0]][coords[1]] = value
+  return matrix
 }
