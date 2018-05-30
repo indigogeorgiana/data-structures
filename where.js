5a84f033f265f3d3643c21f18998f9d82f015b7e
sarah-mclaughlin <sarahmclaughlin0@gmail.com>
2018-05-30 18:35:29 +1200

    working on where function

--- where.js
+++ where.js
@@ -2,12 +2,26 @@ module.exports = where
 
 function where (arr, searchDetails) {
   let newArr = []
+  let findItem
   for (let item in arr) {
+    findItem = true
+    // x = searchDetails keys
     const x = Object.keys(searchDetails)
-    console.log(x)
-    for (var i = 0; i < x.length; i++) {
-      //console.log(x[i])
-      if (arr[item][x][thing] === searchDetails[thing]) {
+    // console.log(x)
+    if (x.length < 2) {
+      if (arr[item][x] === searchDetails[x]) {
+        newArr.push(arr[item])
+      }
+    } else {
+      // y = keys in each object of the array
+      const y = Object.keys(arr[item])
+      for (let v = 0; v < x.length; v++) {
+        console.log(y[v])      
+        if (y[v] !== x[v]) {
+          findItem = false;
+        }
+      }
+      if (findItem === true) {
         newArr.push(arr[item])
       }
     }
