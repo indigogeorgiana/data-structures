d50c69ee245bcaddf26425e0422abf105cd003ba
sarah-mclaughlin <sarahmclaughlin0@gmail.com>
2018-05-30 14:29:02 +1200

    still missing one

--- find.js
+++ find.js
@@ -1,2 +1,10 @@
+module.exports = find
+
 function find (arr, searchDetails) {
+  for (let item in arr) {
+    const x = Object.keys(searchDetails)
+    if (arr[item][x] === searchDetails[x]) {
+      return arr[item]
+    }
+  }
 }
--- where.js
+++ where.js
@@ -1,2 +1,16 @@
+module.exports = where
+
 function where (arr, searchDetails) {
+  let newArr = []
+  for (let item in arr) {
+    const x = Object.keys(searchDetails)
+    console.log(x)
+    for (var i = 0; i < x.length; i++) {
+      //console.log(x[i])
+      if (arr[item][x][thing] === searchDetails[thing]) {
+        newArr.push(arr[item])
+      }
+    }
+  }
+  return newArr
 }
