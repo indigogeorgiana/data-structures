2abf30eae33cc82cd8ce179fe36144da100fc86b
Matthew McGregor <Mattjackmcgregor@gmail.com>
2018-05-30 14:29:15 +1200

    almost

--- matrix.js
+++ matrix.js
@@ -1,4 +1,18 @@
+export module = {
+  getMatrix,
+  updateMatrix
+}
+
 function getMatrix (n) {
+  let arr = []
+  for (i = 0; i < n.length; i++) {
+      
+    for (i= 0; i < n.length; i++) {
+      arr.push(0)
+
+    }
+  }
+  
 }
 
 function updateMatrix (matrix, coords, value) {
